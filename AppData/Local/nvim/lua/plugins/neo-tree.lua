4f691d2aac45e4f4b5937222086bc5993d309cfd
Evtr0na <evtrona@gmail.com>
2026-08-23 12:19:59 +0800

    1

--- AppData/Local/nvim/lua/plugins/neo-tree.lua
+++ AppData/Local/nvim/lua/plugins/neo-tree.lua
@@ -5,10 +5,18 @@ return  {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
+      -- "nvim-tree/nvim-web-devicons",
     },
     opts = {
       close_if_last_window = true,
+      default_component_configs = {
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "",
+          default = "", -- 这里设置默认文件图标，替代 *
+        },
+      },
       filesystem = {
         follow_current_file = {
           enabled = true,
