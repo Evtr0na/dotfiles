d73fcb678582042c1894b5f8165ca561c93c70fc
Evtr0na <evtrona@gmail.com>
2026-08-28 15:10:03 +0800

    '''

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,12 +4,38 @@ local config = wezterm.config_builder()
 -- 字体配置
 config.font = wezterm.font({
 	family = "Iosevka",
+	-- weight = "Regular",
+	-- weight = "Medium",
+	-- weight = "Bold",
+	style = "Normal",
 	harfbuzz_features = { "calt=0", "liga=0", "clig=0" },
 })
+
+-- config.font_rules = {
+-- 	{
+-- 		intensity = "Bold",
+-- 		italic = false,
+-- 		font = wezterm.font({
+-- 			family = "Iosevka",
+-- 			weight = "Bold",
+-- 			harfbuzz_features = {
+-- 				"calt=0",
+-- 				"liga=0",
+-- 				"clig=0",
+-- 			},
+-- 		}),
+-- 	},
+-- }
+
 config.font_size = 14.0
 
+-- 先显式保持默认比例
+config.line_height = 1.0
+config.cell_width = 1.0
+
 -- Windows字体渲染优化，改善Iosevka发虚
-config.freetype_load_target = "Light"
+-- config.freetype_load_target = "Normal"
+config.freetype_load_target = "Mono"
 config.freetype_render_target = "HorizontalLcd"
 
 -- 无标题栏，但保留 Windows 原生拖拽缩放
@@ -33,7 +59,8 @@ config.colors = {
 	cursor_bg = "#CDD6F4", -- 光标背景色（即光标自身的颜色，如鲜红色）
 	-- cursor_border = '#ff5555', -- 光标边框颜色（可选）
 	background = "#181818",
-	foreground = "#e4e4e4",
+	-- foreground = "#e4e4e4",
+	foreground = "#e4e4ef",
 }
 
 return config
